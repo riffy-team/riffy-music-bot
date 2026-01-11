15f21374d6fee18c1ca1de1afb4b63dbe08ef11c
Ramkrishna <ramkrishnagithub@gmail.com>
2026-01-11 15:06:16 +0530

    chore: update configuration with node settings and client ID (token omitted)

--- structures/configuration/index.js
+++ structures/configuration/index.js
@@ -1,15 +1,16 @@
 module.exports = {
     client_token: "",
-    client_id: "",
+    client_id: "1454033736227360860",
     client_prefix: "!",
     mongodb_url: "", //optional
     developers: ["1203605618745933880"],
     sharding: false,
     database: false,
+    defaultSearchPlatform: "ytmsearch",
     nodes: [
         {
-            host: "localhost",
-            port: 2333,
+            host: "lavalink.jirayu.net",
+            port: 13592,
             password: "youshallnotpass",
             secure: false
         }
@@ -20,4 +21,3 @@ module.exports = {
  * Get discord bot token from here https://discord.com/developers/applications
  * Get mongodb url from https://www.mongodb.com/
  */
-
